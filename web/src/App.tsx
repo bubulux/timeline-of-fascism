22c43cfd14943b7a721b8a4b6a2ef02c1f09e4ab
Samuil Berenzon <samus.dev.space@gmail.com>
2026-01-26 19:40:25 +0000

    ff

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,35 @@
-function App() {
-  return <div>Hello World</div>;
-}
+import {
+  MantineProvider,
+  createTheme,
+  type MantineColorsTuple,
+} from "@mantine/core";
 
-export default App;
+import "@mantine/core/styles.css";
+
+const myColor: MantineColorsTuple = [
+  "#ffe8e9",
+  "#ffd1d1",
+  "#fba0a0",
+  "#f76d6d",
+  "#f44141",
+  "#f22625",
+  "#f21616",
+  "#d8070b",
+  "#c10007",
+  "#a90003",
+];
+
+const theme = createTheme({
+  colors: {
+    myColor,
+  },
+  primaryColor: "myColor",
+});
+
+export default function App() {
+  return (
+    <MantineProvider theme={theme} defaultColorScheme="dark">
+      Hello World
+    </MantineProvider>
+  );
+}

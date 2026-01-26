8d2942f970f8df02988ef8f78ecd4b3a607f4ddd
Samuil Berenzon <samus.dev.space@gmail.com>
2026-01-26 20:20:00 +0000

    with storybook

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,38 +1,11 @@
-import {
-  MantineProvider,
-  createTheme,
-  type MantineColorsTuple,
-} from "@mantine/core";
-
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
-
-import "@mantine/core/styles.css";
-
-const myColor: MantineColorsTuple = [
-  "#ffe8e9",
-  "#ffd1d1",
-  "#fba0a0",
-  "#f76d6d",
-  "#f44141",
-  "#f22625",
-  "#f21616",
-  "#d8070b",
-  "#c10007",
-  "#a90003",
-];
-
-const theme = createTheme({
-  colors: {
-    myColor,
-  },
-  primaryColor: "myColor",
-});
+import { ThemeProvider } from "./themeProvider.tsx";
 
 export default function App() {
   return (
-    <MantineProvider theme={theme} defaultColorScheme="dark">
+    <ThemeProvider>
       <RouterProvider router={router} />
-    </MantineProvider>
+    </ThemeProvider>
   );
 }
